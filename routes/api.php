de50b19e2f850b72f68745107d703eefee32412e
raadhiit <radhityaharun45@gmail.com>
2026-07-12 20:51:57 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,6 @@ use App\Http\Controllers\Api\V1\EmployeeProvisioningController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('daidan.api.key')->group(function () {
-    Route::get(
-        '/v1/attendance/sync',
-        [AttendanceSyncController::class, 'index']
-    );
-    Route::put(
-        '/v1/employees/{daidanNik}',
-        [EmployeeProvisioningController::class, 'upsert']
-    );
+    Route::get('/v1/attendance/sync',[AttendanceSyncController::class, 'index']);
+    Route::put('/v1/employees/{daidanNik}',[EmployeeProvisioningController::class, 'upsert']);
 });
