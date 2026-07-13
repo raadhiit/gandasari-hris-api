25ec326ab42af7bf3dc776c88614cc11f684535a
raadhiit <radhityaharun45@gmail.com>
2026-07-13 15:32:36 +0700

    update: integration machine

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+use App\Http\Controllers\FingerprintPushController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::match(
+    ['GET', 'POST'],
+    '/iclock/cdata',
+    [FingerprintPushController::class, 'cdata']
+);
+
+Route::match(
+    ['GET', 'POST'],
+    '/iclock/getrequest',
+    [FingerprintPushController::class, 'getRequest']
+);
+
+Route::match(
+    ['GET', 'POST'],
+    '/iclock/devicecmd',
+    [FingerprintPushController::class, 'deviceCommand']
+);
